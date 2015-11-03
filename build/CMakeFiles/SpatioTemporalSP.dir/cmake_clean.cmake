5d6ccf6feb9f682eb28dcf52fd6d18aa5f092f2e
Mahsa Ghafarian <mahsa.ghafz@gmail.com>
2015-11-03 10:53:13 -0700

    spatiotemporalsp

---
+++ build/CMakeFiles/SpatioTemporalSP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpatioTemporalSP.dir/main.c.o"
+  "SpatioTemporalSP.pdb"
+  "SpatioTemporalSP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpatioTemporalSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
